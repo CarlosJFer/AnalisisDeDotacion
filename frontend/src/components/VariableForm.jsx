eede7311db8e1f825edf52ffa3fca88c23190100
CarlosJFer <105252392+CarlosJFer@users.noreply.github.com>
2025-08-14 12:14:26 -0300

    feat: add variable form component

---
+++ frontend/src/components/VariableForm.jsx
@@ -0,0 +1,177 @@
+import React from 'react';
+import {
+  Grid,
+  Switch,
+  FormControlLabel,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormHelperText
+} from '@mui/material';
+
+const VariableForm = ({
+  values,
+  onChange,
+  type = 'global',
+  unidadOptions = [],
+  secretariaOptions = []
+}) => {
+  const handleChange = (name) => (e) => {
+    const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+    onChange(name, value);
+  };
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={6}>
+        <TextField
+          label="Nombre"
+          value={values.nombre || ''}
+          onChange={handleChange('nombre')}
+          fullWidth
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <FormControl fullWidth>
+          <InputLabel>Unidad de medida</InputLabel>
+          <Select
+            value={values.unidad_medida || ''}
+            label="Unidad de medida"
+            onChange={handleChange('unidad_medida')}
+          >
+            {unidadOptions.map((opt) => (
+              <MenuItem key={opt.value} value={opt.value}>
+                {opt.label}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <TextField
+          label="Valor mínimo"
+          type="number"
+          value={values.valor_minimo || ''}
+          onChange={handleChange('valor_minimo')}
+          fullWidth
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <TextField
+          label="Valor máximo"
+          type="number"
+          value={values.valor_maximo || ''}
+          onChange={handleChange('valor_maximo')}
+          fullWidth
+        />
+      </Grid>
+
+      <Grid item xs={12}>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={Boolean(values.flexible)}
+              onChange={handleChange('flexible')}
+            />
+          }
+          label="Variable flexible"
+        />
+      </Grid>
+
+      {!values.flexible && (
+        <>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label="Umbral crítico"
+              type="number"
+              value={values.umbral_critico || ''}
+              onChange={handleChange('umbral_critico')}
+              fullWidth
+              helperText="Valor a partir del cual se considera saturado"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label="Umbral preventivo"
+              type="number"
+              value={values.umbral_preventivo || ''}
+              onChange={handleChange('umbral_preventivo')}
+              fullWidth
+              helperText="Valor a partir del cual se activa la alerta"
+            />
+          </Grid>
+        </>
+      )}
+
+      {values.flexible && (
+        <>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label="Umbral crítico inferior"
+              type="number"
+              value={values.umbral_critico_inferior || ''}
+              onChange={handleChange('umbral_critico_inferior')}
+              fullWidth
+              helperText="Por debajo del valor ideal"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label="Umbral preventivo inferior"
+              type="number"
+              value={values.umbral_preventivo_inferior || ''}
+              onChange={handleChange('umbral_preventivo_inferior')}
+              fullWidth
+              helperText="Por debajo del valor ideal"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label="Umbral preventivo superior"
+              type="number"
+              value={values.umbral_preventivo_superior || ''}
+              onChange={handleChange('umbral_preventivo_superior')}
+              fullWidth
+              helperText="Por encima del valor ideal"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label="Umbral crítico superior"
+              type="number"
+              value={values.umbral_critico_superior || ''}
+              onChange={handleChange('umbral_critico_superior')}
+              fullWidth
+              helperText="Por encima del valor ideal"
+            />
+          </Grid>
+        </>
+      )}
+
+      {type === 'especifica' && (
+        <Grid item xs={12}>
+          <FormControl fullWidth>
+            <InputLabel>Secretaría</InputLabel>
+            <Select
+              value={values.secretaria || ''}
+              label="Secretaría"
+              onChange={handleChange('secretaria')}
+            >
+              {secretariaOptions.map((opt) => (
+                <MenuItem key={opt.value} value={opt.value}>
+                  {opt.label}
+                </MenuItem>
+              ))}
+            </Select>
+            <FormHelperText>Seleccione la secretaría asociada</FormHelperText>
+          </FormControl>
+        </Grid>
+      )}
+    </Grid>
+  );
+};
+
+export default VariableForm;
+
