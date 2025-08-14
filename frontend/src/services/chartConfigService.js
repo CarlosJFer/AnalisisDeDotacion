a7f2c84df0ee360f36c88c949aae4efeb32417c7
CarlosJFer <105252392+CarlosJFer@users.noreply.github.com>
2025-08-14 13:10:16 -0300

    Add chart config model, controller, routes and service

---
+++ frontend/src/services/chartConfigService.js
@@ -0,0 +1,23 @@
+import apiClient from './api';
+
+const API_URL = '/chart-configs';
+
+const getChartConfigOptions = () => apiClient.get(`${API_URL}/options`);
+const getAllChartConfigs = () => apiClient.get(API_URL);
+const getChartConfigById = (id) => apiClient.get(`${API_URL}/${id}`);
+const createChartConfig = (data) => apiClient.post(API_URL, data);
+const updateChartConfig = (id, data) => apiClient.put(`${API_URL}/${id}`, data);
+const deleteChartConfig = (id) => apiClient.delete(`${API_URL}/${id}`);
+const getChartConfigData = (id) => apiClient.get(`${API_URL}/${id}/data`);
+
+const chartConfigService = {
+  getChartConfigOptions,
+  getAllChartConfigs,
+  getChartConfigById,
+  createChartConfig,
+  updateChartConfig,
+  deleteChartConfig,
+  getChartConfigData,
+};
+
+export default chartConfigService;
