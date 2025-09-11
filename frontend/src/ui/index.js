aab8a219104c59effcc81846e857464baa0796c1
CarlosJFer <105252392+CarlosJFer@users.noreply.github.com>
2025-09-11 09:53:07 -0300

    Refactor stats widget layout

--- frontend/src/ui/index.js
+++ frontend/src/ui/index.js
@@ -1,3 +1,4 @@
 export { default as theme, modeVars } from "./theme.js";
 export { default as icons } from "./icons.js";
+export { default as DashboardCard } from "../components/ui/DashboardCard.jsx";
 export * from "./chart-utils";
